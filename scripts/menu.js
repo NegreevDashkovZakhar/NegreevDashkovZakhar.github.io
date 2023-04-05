1be82450232d0ba47658a60203c8c5b211170597
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-04-05 17:57:26 +0600

    [+]Initial commit

---
+++ scripts/menu.js
@@ -0,0 +1,23 @@
+const changeClass = "change";
+const menuBlock = ` 
+<div class="menu-block" id="menu-block">
+    <a href="index.html">Все песни</a>
+    <a href="artists.html">Все исполнители</a>
+</div>`;
+
+const menuOnClick = () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const menuBlock = document.getElementById("menu-block");
+  menuIcon.classList.toggle(changeClass);
+  menuBlock.classList.toggle(changeClass);
+};
+
+const onLoad = () => {
+  const headerElement = document.getElementById("header");
+  headerElement.innerHTML = `${menuBlock} ${headerElement.innerHTML}`;
+
+  const menuElement = document.getElementById("menu-icon");
+  menuElement.addEventListener("click", menuOnClick);
+};
+
+document.addEventListener("DOMContentLoaded", onLoad);
---
+++ scripts/prompt.js
@@ -0,0 +1,23 @@
+const promptId = "prompt-id";
+
+const closePrompt = () => {
+  document.getElementById(promptId).remove();
+};
+
+const showPrompt = (title, text) => {
+  const bodyElement = document.getElementsByTagName("body")[0];
+  const promptElement = document.createElement("div");
+  promptElement.className = "prompt-background";
+  promptElement.id = promptId;
+  promptElement.addEventListener("click", closePrompt);
+
+  promptElement.innerHTML = `
+    <div class="prompt">
+      <h5 class="prompt__title">${title}</h5>
+      <p class="prompt__text">${text}</p>
+    </div>
+  `;
+  bodyElement.appendChild(promptElement);
+};
+
+export { showPrompt };
