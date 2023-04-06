da3459988369bc48a887b5bb2216294bcf27e484
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-04-06 23:54:09 +0600

    [+]Added artists rendering

--- scripts/artists.js
+++ scripts/artists.js
@@ -1,14 +1,12 @@
 const renderArtists = async () => {
-    const result = await fetch('http://localhost/php/songs/all.php');
-    const songs = await result.json();
+    const result = await fetch('http://localhost/php/artists/songs_count.php');
+    const artists = await result.json();
     const entries_list = document.getElementById('entries_list');
-    songs.forEach(song => {
+    artists.forEach(artist => {
       entries_list.innerHTML += `<div class="list__entry">
-      <img class="entry__image" src="./images/artists/${song.artist_name}.jpg" />
-      <a class="entry__song">"${song.song_name}"</a>
-      <span> - </span>
-      <a class="entry__author">${song.artist_name}</a>
-      <p class="entry__year">${song.song_year}</p>
+      <img class="entry__image" src="./images/artists/${artist.artist_name}.jpg" />
+      <a class="entry__author">${artist.artist_name}</a>
+      <p class="entry__right_column">${artist.songs_count}</p>
     </div>`
     });
 }
---
+++ scripts/songs.js
@@ -0,0 +1,16 @@
+const renderSongs = async () => {
+    const result = await fetch('http://localhost/php/songs/all.php');
+    const songs = await result.json();
+    const entries_list = document.getElementById('entries_list');
+    songs.forEach(song => {
+      entries_list.innerHTML += `<div class="list__entry">
+      <img class="entry__image" src="./images/artists/${song.artist_name}.jpg" />
+      <a class="entry__song">"${song.song_name}"</a>
+      <span> - </span>
+      <a class="entry__author">${song.artist_name}</a>
+      <p class="entry__right_column">${song.song_year}</p>
+    </div>`
+    });
+}
+
+window.addEventListener('DOMContentLoaded',renderSongs);
