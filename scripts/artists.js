60fba9083d7b3805ee96712bf09ce24e79cae32b
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-04-07 23:13:09 +0600

    [+]Added search functions

--- scripts/artists.js
+++ scripts/artists.js
@@ -4,7 +4,15 @@ const renderArtists = async () => {
   const result = await fetch(`${base_url}/php/artists/songs_count.php`);
   const artists = await result.json();
   const entries_list = document.getElementById("entries_list");
+  entries_list.innerHTML = '<p class="list__year">Песен</p>';
+  
+  const searchedValue = document.getElementById("search-input").value;
+
   artists.forEach((artist) => {
+    const wasSearched = artist.artist_name.toUpperCase().includes(searchedValue.toUpperCase());
+    if (!wasSearched) {
+      return;
+    }
     entries_list.innerHTML += `<div class="list__entry">
       <img class="entry__image" src="./images/artists/${artist.artist_name}.jpg" />
       <a class="entry__author" href="${base_url}/artist_page.html?artist_name=${artist.artist_name}">
@@ -15,4 +23,10 @@ const renderArtists = async () => {
   });
 };
 
-window.addEventListener("DOMContentLoaded", renderArtists);
+const onLoad = () => {
+  const searchButton = document.getElementById("search-button");
+  searchButton.addEventListener("click",renderArtists);
+  renderArtists();
+}
+
+window.addEventListener("DOMContentLoaded", onLoad);
--- scripts/songs.js
+++ scripts/songs.js
@@ -1,11 +1,19 @@
 import base_url from "./config.js";
 
 const renderSongs = async () => {
-    const result = await fetch(`${base_url}/php/songs/all.php`);
-    const songs = await result.json();
-    const entries_list = document.getElementById('entries_list');
-    songs.forEach(song => {
-      entries_list.innerHTML += `<div class="list__entry">
+  const result = await fetch(`${base_url}/php/songs/all.php`);
+  const songs = await result.json();
+  const entries_list = document.getElementById("entries_list");
+  entries_list.innerHTML = '<p class="list__year">Год песни</p>';
+
+  const searchedValue = document.getElementById("search-input").value;
+
+  songs.forEach((song) => {
+    const wasSearched = song.song_name.toUpperCase().includes(searchedValue.toUpperCase());
+    if (!wasSearched) {
+      return;
+    }
+    entries_list.innerHTML += `<div class="list__entry">
       <img class="entry__image" src="./images/artists/${song.artist_name}.jpg" />
       <a class="entry__song" href="${base_url}/song_page.html?id=${song.id}">"${song.song_name}"</a>
       <span> - </span>
@@ -13,8 +21,14 @@ const renderSongs = async () => {
         ${song.artist_name}
       </a>
       <p class="entry__right_column">${song.song_year}</p>
-    </div>`
-    });
-}
+      </div>`;
+  });
+};
 
-window.addEventListener('DOMContentLoaded',renderSongs);
+const onLoad = () => {
+  const searchButton = document.getElementById("search-button");
+  searchButton.addEventListener("click",renderSongs);
+  renderSongs();
+};
+
+window.addEventListener("DOMContentLoaded", onLoad);
