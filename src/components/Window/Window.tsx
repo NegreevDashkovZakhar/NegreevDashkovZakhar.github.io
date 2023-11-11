d4327d6e12b32288f52db4d2ff8b07f3bcf1d5eb
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-11-11 14:35:47 +0600

    feat: added window container component

---
+++ src/components/Window/Window.tsx
@@ -0,0 +1,17 @@
+import style from './Window.module.scss';
+import classNames from 'classnames';
+
+interface WindowProps {
+  children: React.ReactElement;
+  className: string;
+}
+
+const Window: React.FC<WindowProps> = ({children, className = ''}) => {
+  return (
+    <div className={classNames(style.window, className)}>
+      <div className={style['window__content']}>{children}</div>
+    </div>
+  );
+};
+
+export default Window;
