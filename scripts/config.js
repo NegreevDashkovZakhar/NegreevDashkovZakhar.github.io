6f93f6b1a065ae2e97ea95c8305afe6cb0680292
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2024-01-05 02:23:12 +0600

    config

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
-// export const base_url = 'https://cors-anywhere.herokuapp.com/http://31.184.253.173/php/tabs';
-export const base_url = 'http://localhost/php';
-export const site_url = 'http://localhost';
+export const base_url = 'https://cors-anywhere.herokuapp.com/http://31.184.253.173/php/tabs';
+// export const base_url = 'http://localhost/php';
+// export const site_url = 'http://localhost';
 
-// export const site_url = 'https://negreevdashkovzakhar.github.io';
+export const site_url = 'https://negreevdashkovzakhar.github.io';
