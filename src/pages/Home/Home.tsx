8bc4a69f81eaa31b3ef49bcf88bbf6d8bb1b3f42
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2024-02-11 16:31:47 +0600

    feat: home title

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,7 @@ import style from './Home.module.scss';
 const Home: React.FC = () => {
   return (
     <div className={style['home']}>
+      <p className={style['home__title']}>Это мы гуляем?</p>
       <Picture />
     </div>
   );
